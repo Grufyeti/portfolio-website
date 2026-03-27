932a1bb1d8c2d47067127826bbf10192ac3aad38
Grufyeti <martinkopoky@gmail.com>
2026-03-27 11:15:07 +0100

    Use esbuild CSS minification for mobile compatibility

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    minify: 'esbuild',
+    cssMinify: 'esbuild',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
